142a5b3c35281507f61890d5695acb1d45989daf
Shudipto Trafder <shudiptotrafder@gmail.com>
2024-07-26 19:59:36 +0600

    added taskiq

--- fastapi_injector/attach.py
+++ fastapi_injector/attach.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from injector import Injector, InstanceProvider, singleton
+from taskiq import TaskiqState
 
 from fastapi_injector.exceptions import InjectorNotAttached
 from fastapi_injector.request_scope import RequestScopeFactory, RequestScopeOptions
 
 
 def attach_injector(
-    app: FastAPI,
-    injector: Injector,
-    options: RequestScopeOptions = RequestScopeOptions(),
+        app: FastAPI,
+        injector: Injector,
+        options: RequestScopeOptions = RequestScopeOptions(),
 ) -> None:
     """
     Call this function on app startup to attach an injector to the app.
@@ -26,6 +27,33 @@ def get_injector_instance(app: FastAPI) -> Injector:
     """
     try:
         return app.state.injector
+    except AttributeError as exc:
+        raise InjectorNotAttached(
+            "No injector instance has been attached to the app."
+        ) from exc
+
+
+def attach_injector_taskiq(
+        state: TaskiqState,
+        injector: Injector,
+        options: RequestScopeOptions = RequestScopeOptions(),
+) -> None:
+    """
+    Call this function on taskiq startup to attach an injector to the taskiq.
+    """
+    state.injector = injector
+    injector.binder.bind(
+        RequestScopeOptions, InstanceProvider(options), scope=singleton
+    )
+    injector.binder.bind(RequestScopeFactory, to=RequestScopeFactory, scope=singleton)
+
+
+def get_injector_instance_taskiq(state: TaskiqState, ) -> Injector:
+    """
+    Returns the injector instance attached to the taskiq.
+    """
+    try:
+        return state.injector
     except AttributeError as exc:
         raise InjectorNotAttached(
             "No injector instance has been attached to the app."
--- fastapi_injector/injected.py
+++ fastapi_injector/injected.py
@@ -1,14 +1,14 @@
-from typing import Any, TypeVar
-
 from fastapi import Depends
 from starlette.requests import HTTPConnection
+from taskiq import TaskiqDepends, Context
+from typing import Any, TypeVar, Generic, Annotated
 
-from fastapi_injector.attach import get_injector_instance
+from fastapi_injector.attach import get_injector_instance, get_injector_instance_taskiq
 
 BoundInterface = TypeVar("BoundInterface", bound=type)
 
 
-def Injected(interface: BoundInterface) -> Any:  # pylint: disable=invalid-name
+def Injected(interface: Generic[BoundInterface]) -> BoundInterface:  # pylint: disable=invalid-name
     """
     Asks your injector instance for the specified type,
     allowing you to use it in the route.
@@ -20,7 +20,7 @@ def Injected(interface: BoundInterface) -> Any:  # pylint: disable=invalid-name
     return Depends(inject_into_route)
 
 
-def SyncInjected(interface: BoundInterface) -> Any:  # pylint: disable=invalid-name
+def SyncInjected(interface: Generic[BoundInterface]) -> BoundInterface:  # pylint: disable=invalid-name
     """
     Asks your injector instance for the specified type,
     allowing you to use it in the route. Intended for use
@@ -31,3 +31,28 @@ def SyncInjected(interface: BoundInterface) -> Any:  # pylint: disable=invalid-n
         return get_injector_instance(conn.app).get(interface)
 
     return Depends(inject_into_route)
+
+
+def InjectedTaskiq(interface: Generic[BoundInterface]) -> BoundInterface:  # pylint: disable=invalid-name
+    """
+    Asks your injector instance for the specified type,
+    allowing you to use it in the task.
+    """
+
+    async def inject_into_task(context: Annotated[Context, TaskiqDepends()]) -> BoundInterface:
+        return get_injector_instance_taskiq(context.state).get(interface)
+
+    return TaskiqDepends(inject_into_task)
+
+
+def SyncInjectedTaskiq(interface: Generic[BoundInterface]) -> BoundInterface:  # pylint: disable=invalid-name
+    """
+    Asks your injector instance for the specified type,
+    allowing you to use it in the route. Intended for use
+    with synchronous interfaces.
+    """
+
+    def inject_into_task(context: Annotated[Context, TaskiqDepends()]) -> BoundInterface:
+        return get_injector_instance_taskiq(context.state).get(interface)
+
+    return TaskiqDepends(inject_into_task)
