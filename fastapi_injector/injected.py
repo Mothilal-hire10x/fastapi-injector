1d765334e2724eaf4810e3bb5092890ab73fb37a
Shudipto Trafder <shudiptotrafder@gmail.com>
2025-05-20 00:09:02 +0600

    refactor: update type annotations in injected.py for consistency and clarity

--- fastapi_injector/injected.py
+++ fastapi_injector/injected.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Generic, TypeVar
+from typing import Annotated, Generic, TypeVar, Type
 
 from fastapi import Depends
 from starlette.requests import HTTPConnection
@@ -6,41 +6,41 @@ from taskiq import Context, TaskiqDepends
 
 from fastapi_injector.attach import get_injector_instance, get_injector_instance_taskiq
 
-BoundInterface = TypeVar("BoundInterface", bound=type)
+T = TypeVar("T")
 
 
 def Injected(
-    interface: Generic[BoundInterface],
-) -> BoundInterface:  # pylint: disable=invalid-name
+    interface: Type[T],
+) -> T:  # pylint: disable=invalid-name
     """
     Asks your injector instance for the specified type,
     allowing you to use it in the route.
     """
 
-    async def inject_into_route(conn: HTTPConnection) -> BoundInterface:
+    async def inject_into_route(conn: HTTPConnection) -> T:
         return get_injector_instance(conn.app).get(interface)
 
     return Depends(inject_into_route)
 
 
 def SyncInjected(
-    interface: Generic[BoundInterface],
-) -> BoundInterface:  # pylint: disable=invalid-name
+    interface: Type[T],
+) -> T:  # pylint: disable=invalid-name
     """
     Asks your injector instance for the specified type,
     allowing you to use it in the route. Intended for use
     with synchronous interfaces.
     """
 
-    def inject_into_route(conn: HTTPConnection) -> BoundInterface:
+    def inject_into_route(conn: HTTPConnection) -> T:
         return get_injector_instance(conn.app).get(interface)
 
     return Depends(inject_into_route)
 
 
 def InjectedTaskiq(
-    interface: Generic[BoundInterface],
-) -> BoundInterface:  # pylint: disable=invalid-name
+    interface: Type[T],
+) -> T:  # pylint: disable=invalid-name
     """
     Asks your injector instance for the specified type,
     allowing you to use it in the task.
@@ -48,15 +48,15 @@ def InjectedTaskiq(
 
     async def inject_into_task(
         context: Annotated[Context, TaskiqDepends()],
-    ) -> BoundInterface:
+    ) -> T:
         return get_injector_instance_taskiq(context.state).get(interface)
 
     return TaskiqDepends(inject_into_task)
 
 
 def SyncInjectedTaskiq(
-    interface: Generic[BoundInterface],
-) -> BoundInterface:  # pylint: disable=invalid-name
+    interface: Type[T],
+) -> T:  # pylint: disable=invalid-name
     """
     Asks your injector instance for the specified type,
     allowing you to use it in the route. Intended for use
@@ -65,7 +65,7 @@ def SyncInjectedTaskiq(
 
     def inject_into_task(
         context: Annotated[Context, TaskiqDepends()],
-    ) -> BoundInterface:
+    ) -> T:
         return get_injector_instance_taskiq(context.state).get(interface)
 
     return TaskiqDepends(inject_into_task)
